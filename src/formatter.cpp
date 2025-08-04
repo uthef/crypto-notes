6fc49c84a16fab716b00574c2196f99613f3f9c6
Nightcrawler <denbrylski@gmail.com>
2025-08-04 20:29:02 +0300

    date formatter fix

--- src/formatter.cpp
+++ src/formatter.cpp
@@ -8,40 +8,32 @@ using namespace cryptonotes;
 const char* MONTHS[] = { "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec" };
 
 QString Formatter::timestampToReadableDateTime(long timestamp) {
-    time_t tt(timestamp);
-    tm tm;
-
-#if WIN == 1
-    localtime_s(&tm, &tt);
-#else
-    localtime_r(&tt, &tm);
-#endif
-
+    QDateTime dt = QDateTime::fromSecsSinceEpoch(timestamp);
     QDate currentDate = QDateTime::currentDateTime().date();
     QString str;
 
-    if (tm.tm_year + 1900 != currentDate.year()) {
+    if (dt.date().year() != currentDate.year()) {
         str = QString("%1, %2 %3, %4:%5")
-                  .arg(tm.tm_year + 1900)
-                  .arg(MONTHS[tm.tm_mon]).arg(tm.tm_mday)
-                  .arg(tm.tm_hour, 2, 10, QChar('0'))
-                  .arg(tm.tm_min, 2, 10, QChar('0'));
+                  .arg(dt.date().year())
+                  .arg(MONTHS[dt.date().month() - 1]).arg(dt.date().day())
+                  .arg(dt.time().hour(), 2, 10, QChar('0'))
+                  .arg(dt.time().minute(), 2, 10, QChar('0'));
 
         return str;
     }
 
-    if (tm.tm_mday != currentDate.day() || tm.tm_mon != currentDate.month()) {
+    if (dt.date().day() != currentDate.day() || dt.date().month() != currentDate.month()) {
         str = QString("%1 %2, %3:%4")
-                  .arg(MONTHS[tm.tm_mon]).arg(tm.tm_mday)
-                  .arg(tm.tm_hour, 2, 10, QChar('0'))
-                  .arg(tm.tm_min, 2, 10, QChar('0'));
+                  .arg(MONTHS[dt.date().month() - 1]).arg(dt.date().day())
+                  .arg(dt.time().hour(), 2, 10, QChar('0'))
+                  .arg(dt.time().minute(), 2, 10, QChar('0'));
 
         return str;
     }
 
     return QString("%1:%2")
-        .arg(tm.tm_hour, 2, 10, QChar('0'))
-        .arg(tm.tm_min, 2, 10, QChar('0'));
+        .arg(dt.time().hour(), 2, 10, QChar('0'))
+        .arg(dt.time().minute(), 2, 10, QChar('0'));
 }
 
 QString Formatter::escapeQuery(QString query, char escapeChar) {
